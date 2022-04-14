e2941723412503ef735399ed62b20b85b241202f
kronosGR <geo.elgeo@gmail.com>
2022-04-14 17:53:42 +0200

    fixed some style

--- components/Layout/Navigation/NavAdmin.js
+++ components/Layout/Navigation/NavAdmin.js
@@ -2,14 +2,27 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+import styles from '../../../styles/NavAdmin.module.css';
+
 export default function NavAdmin() {
   return (
-    <nav>
-      <Link href='/index.js'>
+    <nav className={styles.nav}>
+      <Link href='/'>
         <a>
-          <Image src='/icons/Logo.svg' width={50} height={100} />
+          <Image src='/icons/logo.png' width={48} height={28} />
         </a>
       </Link>
+      <ul className={styles.nav_list}>
+        <li>Home</li>
+        <li>
+          <Link href='/accommodations'>
+            <a>Accommodations</a>
+          </Link>
+        </li>
+        <li>Contact</li>
+        <li>Favorites</li>
+        <li>Login</li>
+      </ul>
     </nav>
   );
 }
