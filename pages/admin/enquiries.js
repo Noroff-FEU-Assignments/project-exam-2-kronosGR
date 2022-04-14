75ebea661abd504edb503e3aa913cebd76b89479
kronosGR <geo.elgeo@gmail.com>
2022-04-14 13:10:22 +0200

    created pages and directory structure

---
+++ pages/admin/enquiries.js
@@ -0,0 +1,6 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Enquiries() {
+  return <div>Enquiries</div>;
+}
---
+++ pages/admin/messages.js
@@ -0,0 +1,6 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Messages() {
+  return <div>Messages</div>;
+}
