320caf01145df7a4674f9650d3472c638820ea4a
kronosGR <geo.elgeo@gmail.com>
2022-04-15 20:00:57 +0200

    added active class to link

--- components/Layout/Navigation/NavigationItem.js
+++ components/Layout/Navigation/NavigationItem.js
@@ -1,14 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import styles from '../../../styles/NavigationItem.module.css';
 
 export default function NavigationItem({ href, src, title, alt, width, height }) {
+  const router = useRouter();
   return (
     <li>
       <Link href={href}>
-        <a className={styles.nav_a}>
+        <a className={`${router.pathname === href ? 'active' : ''} ${styles.nav_a}`}>
           <Image src={src} alt={alt} width={width} height={height} className='red mh5' />
           {title}
         </a>
