535779b48ee9890ac6d8775415ee92b969128542
kronosGR <geo.elgeo@gmail.com>
2022-04-16 19:58:35 +0200

    footer finished. Added some icons as components

--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -1,33 +1,24 @@
 import React from 'react';
 
 import styles from '../../../styles/Footer.module.css';
+import Facebook from '../Icons/Facebook';
 import FooterLink from './FooterLink';
+import Twitter from '../Icons/Twitter';
+import Instagram from '../Icons/Instagram';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.social}>
-        <FooterLink
-          href='#'
-          icon='/icons/facebook.svg'
-          width={27}
-          height={27}
-          alt='facebook'
-        />
-        <FooterLink
-          href='#'
-          icon='/icons/twitter.svg'
-          width={27}
-          height={27}
-          alt='twitter'
-        />
-        <FooterLink
-          href='#'
-          icon='/icons/instagram.svg'
-          width={27}
-          height={27}
-          alt='instagram'
-        />
+        <FooterLink href='#'>
+          <Facebook width={27} height={27} />
+        </FooterLink>
+        <FooterLink href='#'>
+          <Twitter width={27} height={27} />
+        </FooterLink>
+        <FooterLink href='#'>
+          <Instagram width={27} height={27} />
+        </FooterLink>
       </div>
       <div className={styles.copyright}>
         @ 2020 - Georgios Katsanakis (A.K.A KronosGR)
--- components/Layout/Footer/FooterLink.js
+++ components/Layout/Footer/FooterLink.js
@@ -3,13 +3,11 @@ import Link from 'next/link';
 import React from 'react';
 
 import styles from '../../../styles/FooterLink.module.css';
-export default function FooterLink({ href, icon, alt, width, height }) {
+export default function FooterLink({ href, children }) {
   return (
     <div>
       <Link href={href}>
-        <a className={styles.container}>
-          <Image src={icon} width={width} height={height} alt={alt} />
-        </a>
+        <a className={styles.container}>{children}</a>
       </Link>
     </div>
   );
