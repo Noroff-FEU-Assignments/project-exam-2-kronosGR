b7b068c437e5f912594cf57864092e53e86a5648
kronosGR <geo.elgeo@gmail.com>
2022-04-22 21:20:17 +0200

    added updateFavorites

---
+++ BackEnd/updateFavorites.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { ACCOMMODATIONS, API_URL } from '../constants/Api';
+
+/**
+ * update favorites
+ * @param {*} aid accommodation id
+ * @param {*} favorites favorites amount
+ */
+export const updateFavorites = async (aid, favorites) => {
+  let ret = { result: '', error: '' };
+
+  const body = {
+    favorites,
+  };
+  try {
+    const res = await axios.put(API_URL + ACCOMMODATIONS + '/' + aid, body);
+
+    if (res.error) {
+      throw new Error('Sorry something went wrong');
+    }
+    ret.result = res.data;
+  } catch (err) {
+    ret.error = err.stack;
+  }
+
+  return ret;
+};
