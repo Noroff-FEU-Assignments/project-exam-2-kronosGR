10139a770af75a3711b7aef1d8eef340134364fd
kronosGR <geo.elgeo@gmail.com>
2022-04-23 17:33:53 +0200

    finished carousel

--- components/Accommodations/AccommodationCarousel.js
+++ components/Accommodations/AccommodationCarousel.js
@@ -1,32 +1,47 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from '../../styles/AccommodationCarousel.module.css';
 import { API_URL } from '../../constants/Api';
 
 export const AccommodationCarousel = ({ item }) => {
-  console.log(item.images);
+  const [active, setActive] = useState(0);
+  const length = item.images.length;
+
+  const handleClick = (e) => {
+    const type = e.target.dataset.kind;
+    if (type === 'next') setActive(active + 1);
+    if (type === 'prev') setActive(active - 1);
+
+    if (active < 0) setActive(length - 2);
+    if (active > length - 2) setActive(0);
+  };
+
   return (
     <div className={styles.image_container}>
       <div className={styles.carousel}>
         {item.images.map((item, idx) => {
           return (
-            <Image
-              key={idx}
-              className={styles.image}
-              src={API_URL + item.formats.medium.url}
-              layout='fill'
-              alt={item.name}
-            />
+            <>
+              {active === idx && (
+                <Image
+                  key={idx}
+                  className={styles.image}
+                  src={API_URL + item.formats.medium.url}
+                  layout='fill'
+                  alt={item.name}
+                />
+              )}
+            </>
           );
         })}
       </div>
       <div className={styles.overlay}>
-        <a href='' className={styles.previous}>
-          <img src='/icons/prev.svg' />
+        <a href='#' className={styles.previous} data-kind='prev' onClick={handleClick}>
+          <img src='/icons/prev.svg' data-kind='prev' alt='previous' />
         </a>
-        <a href='' className={styles.next}>
-          <img src='/icons/next.svg' />
+        <a href='#' className={styles.next} onClick={handleClick}>
+          <img src='/icons/next.svg' data-kind='next' alt='next' />
         </a>
       </div>
     </div>
