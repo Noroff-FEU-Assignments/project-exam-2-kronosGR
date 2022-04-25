983aa5997ea1df55a34d99beee8560f278c1a8dd
kronosGR <geo.elgeo@gmail.com>
2022-04-25 14:14:38 +0200

    added acc info

--- pages/enquire.js
+++ pages/enquire.js
@@ -2,9 +2,26 @@ import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { getAccommodationById } from '../BackEnd/getAccommodationById';
+import Spacer from '../components/Layout/Spacer';
+import Layout from '../components/Layout/Layout';
+import { AccommodationCarousel } from '../components/Accommodations/AccommodationCarousel';
+
+import styles from '../styles/enquire.module.css';
 
 export default function Enquire({ accommodation, error }) {
-  return <div>Enquire</div>;
+  return (
+    <Layout>
+      <Spacer size={30} />
+      <h1>{accommodation.name}</h1>
+      <div className={styles.container}>
+        <AccommodationCarousel item={accommodation} />
+        <p className={styles.description}>{`${accommodation.description.substr(
+          0,
+          100
+        )}...`}</p>
+      </div>
+    </Layout>
+  );
 }
 
 export async function getServerSideProps(context) {
