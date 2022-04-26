84fae2c16d80b37086413946f79e401acc31bf3c
kronosGR <geo.elgeo@gmail.com>
2022-04-26 17:09:29 +0200

    added on click enlarge

--- components/Messages/MessageItem.js
+++ components/Messages/MessageItem.js
@@ -1,14 +1,29 @@
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from '../../styles/MessageItem.module.css';
 
 export const MessageItem = ({ item }) => {
   const [msg, setMsg] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
 
-  console.log(item);
+  useEffect(() => {
+    setMsg(item.message.substr(0, 150) + '...');
+  }, []);
+
+  const clickHandler = () => {
+    setIsVisible(!isVisible);
+    if (!isVisible) {
+      setMsg(item.message);
+    } else {
+      setMsg(item.message.substr(0, 150) + '...');
+    }
+
+    //TODO on click check if read if not read update to read
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={clickHandler}>
       <div className={styles.title_container}>
         {item.read && (
           <Image src='/icons/bell-outline.svg' width={30} height={30} alt={item.name} />
@@ -20,6 +35,13 @@ export const MessageItem = ({ item }) => {
         <span className={styles.name}>{item.name}</span>
       </div>
       <p className={styles.message}>{msg}</p>
+      {isVisible && (
+        <div className={styles.container_extra}>
+          <span className={styles.extra}>{item.email}</span>
+          <span className={styles.extra}>{item.phone}</span>
+          <span className={styles.extra}>{item.published_at}</span>
+        </div>
+      )}
     </div>
   );
 };
--- components/Messages/MessageList.js
+++ components/Messages/MessageList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { MessageItem } from './MessageItem';
 
+import styles from '../../styles/MessagesList.module.css';
+
 export const MessageList = ({ messages }) => {
   return (
-    <div>
+    <div className={styles.container}>
       {messages && messages.map((item, i) => <MessageItem item={item} key={i} />)}
     </div>
   );
