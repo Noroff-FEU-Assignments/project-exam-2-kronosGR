737f5e1c5b7df8ac9bdca3647761b8c0993466ef
kronosGR <geo.elgeo@gmail.com>
2022-04-27 08:45:06 +0200

    added comments on localstorage

--- utils/localStorage.js
+++ utils/localStorage.js
@@ -1,16 +1,30 @@
 export const USER = 'user';
 export const FAVORITES = 'favorites';
 
+/**
+ * save to local storage
+ * @param {*} key name of key
+ * @param {*} value value to store
+ */
 export const saveToLocalStorage = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
+/**
+ * load from local storage
+ * @param {*} key name of key
+ * @returns the information stored in local storage
+ */
 export const loadFromLocalStorage = (key) => {
   if (typeof window !== 'undefined') {
     return JSON.parse(localStorage.getItem(key)) || {};
   }
 };
 
+/**
+ * check if logged in
+ * @returns if is logged in
+ */
 export const checkIfLoggedIn = () => {
   if (typeof window !== 'undefined') {
     if (localStorage.getItem(USER) === null) return false;
@@ -18,12 +32,20 @@ export const checkIfLoggedIn = () => {
   }
 };
 
+/**
+ * Remove from local storage
+ * @param {*} key to remove
+ */
 export const removeFromLocalStorage = (key) => {
   if (typeof window !== 'undefined') {
     localStorage.removeItem(key);
   }
 };
 
+/**
+ * Toggles an accommodation's favorite
+ * @param {*} id of accommodation to toggle
+ */
 export const toggleFavorites = (id) => {
   const favorites = loadFromLocalStorage(FAVORITES) || {};
   const enabled = favorites[id] || false;
@@ -32,6 +54,11 @@ export const toggleFavorites = (id) => {
   saveToLocalStorage(FAVORITES, favorites);
 };
 
+/**
+ * checks if an accommodation is in favorites
+ * @param {*} id of the accommodation
+ * @returns true|false
+ */
 export const checkIfIsFavorite = (id) => {
   const favorites = loadFromLocalStorage(FAVORITES) || {};
   let isFavorite = false;
